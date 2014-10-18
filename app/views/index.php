4f351f9b55b99cced020ec5788a7894ee4b94776
Wil Birkmaier <wil@birkmaier.org>
2014-10-18 11:35:55 -0400

    Added links to lorem and user generator reflected in routes

--- app/views/index.php
+++ app/views/index.php
@@ -20,11 +20,13 @@
 	<h1>Developer's Best Friend</h1>
 	<br>
 	<h2>Lorem Ipsum</h2>
-	<p>Lorem Ipsum is dummy text that is used in order to facilitate page layouts, where text is needed, but the focus is on the layout rather than the content.</p>
+	<p>Lorem Ipsum is dummy text that is used in order to facilitate page layouts, where text is needed, but the focus is on the layout rather than the content. Most viewers have a tendancy to focus on the text rather than the layout, this prevents that.</p>
+	<a href='/lorem-ipsum-generate'>Keep Calm and Generate On</a>
 	<br>
 	<h2>Random User</h2>
 	<p>Sometimes you need the ability to populate a database with random user information.</p>
 	<br>
+	<a href='/random-user-generate'>Keep Calm and Generate On</a>
 
 </body>
 
