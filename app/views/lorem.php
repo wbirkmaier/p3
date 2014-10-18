6c5ef3ac4ca091ae2d9cfd9ad1d9dacc26e5bcc7
Wil Birkmaier <wil@birkmaier.org>
2014-10-18 13:05:09 -0400

    Put in tempory php code to test lorem generator

--- app/views/lorem.php
+++ app/views/lorem.php
@@ -19,6 +19,13 @@
 <body>
 	<h1>Lorem Ipsum</h1>
 	<p>Lorem Ipsum is dummy text that is used in order to facilitate page layouts, where text is needed, but the focus is on the layout rather than the content. Most viewers have a tendancy to focus on the text rather than the layout, this prevents that.</p>
+	<p>
+	<?php
+		$generator = new Badcow\LoremIpsum\Generator();
+		$paragraphs = $generator->getParagraphs(5);
+		echo implode('<p>', $paragraphs);
+	?>
+	</p>
 	<p>You can <a href='/'>Return Home</a> from this place.</p>
 
 </body>
