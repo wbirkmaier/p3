81de1f3caf40f02555b5057180db525f1284d860
Wil Birkmaier <wil@birkmaier.org>
2014-10-18 17:20:29 -0400

    skeleton for user generation view enabled

--- app/views/user.php
+++ app/views/user.php
@@ -21,29 +21,17 @@
 	<p>Sometimes you need the ability to populate a database with random user information.</p>
 	<p>
 	<?php
-	// require the Faker autoloader
-	require_once '/var/www/html/p3/vendor/fzaninotto/faker/src/autoload.php';
-	// alternatively, use another PSR-0 compliant autoloader (like the Symfony2 ClassLoader for instance)
+	//Load Faker Library
+	$fakerPath = app_path().'/../vendor/fzaninotto/faker/src/autoload.php';
+	require_once $fakerPath;
 
 	// use the factory to create a Faker\Generator instance
 	$faker = Faker\Factory::create();
 
 	// generate data by accessing properties
 	echo $faker->name;
-	  // 'Lucy Cechtelar';
 	echo $faker->address;
-	  // "426 Jordy Lodge
-	  // Cartwrightshire, SC 88120-6700"
 	echo $faker->text;
-	  // Sint velit eveniet. Rerum atque repellat voluptatem quia rerum. Numquam excepturi
-	  // beatae sint laudantium consequatur. Magni occaecati itaque sint et sit tempore. Nesciunt
-	  // amet quidem. Iusto deleniti cum autem ad quia aperiam.
-	  // A consectetur quos aliquam. In iste aliquid et aut similique suscipit. Consequatur qui
-	  // quaerat iste minus hic expedita. Consequuntur error magni et laboriosam. Aut aspernatur
-	  // voluptatem sit aliquam. Dolores voluptatum est.
-	  // Aut molestias et maxime. Fugit autem facilis quos vero. Eius quibusdam possimus est.
-	  // Ea quaerat et quisquam. Deleniti sunt quam. Adipisci consequatur id in occaecati.
-	  // Et sint et. Ut ducimus quod nemo ab voluptatum.
 	?>
 
 	</p>
